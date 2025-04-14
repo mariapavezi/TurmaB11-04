1450ba6378c2c7aaeda9ca8788f12e685f113d06
mariapavezi <maria.luisa.pavezi.08@gmail.com>
2025-04-14 00:00:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,10 @@
 function expressao(parametro) {
-    document.getElementById('Display-calculadora').value += parametro; /*
+    document.getElementById('display').value += parametro; /*
     let valor = document.getElementById('Display-calculadora').value;
     valor = valor + parametro;
     document.getElementById('Display-calculadora').value = valor;*/
 }
 
-function calcular(){
-    //eval() 
-    try{
-        let resultado = eval(document.getElementById('Display-calculadora').value);
-        document.getElementById('Display-calculadora').value = resultado;
-    }catch(error){
-        document.getElementById('Display-calculadora').value = "Expressão incorreta.";
-    }
-}
-
 function limpar(){
-    document.getElementById('Display-calculadora').value = "";
-}
-
-function apagarUltimo() {
-    let display = document.getElementById('Display-calculadora');
-    display.value = display.value.slice(0, -1);
-}
-
-function paraNegativo() {
-    let display = document.getElementById('Display-calculadora');
-    display.value = (parseFloat(display.value)*-1);
-}
-
-function porCento() {
-    let display = document.getElementById('Display-calculadora');
-    display.value = (parseFloat(display.value)/100);
+    document.getElementById('display').value = "";
 }
